6192a352ce41e6e477fcbf6317344115ce0b5e9b
NT <usedforfunny@gmail.com>
2022-09-29 13:29:48 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Alexandre Talatinian on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
